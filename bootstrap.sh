21630867e1b7f8c3c261e7c6e437be04a46b85c4
Adam Shumann <ashumann13@gmail.com>
2017-08-02 11:37:37 -0500

    Disable npm prefixing, moved nvm install

--- bootstrap.sh
+++ bootstrap.sh
@@ -54,7 +54,6 @@ pushd $HOME
 	ZSH_CUSTOM=~/.oh-my-zsh/custom
 	mkdir -p $ZSH_CUSTOM/themes
 	curl -o $ZSH_CUSTOM/themes/spaceship.zsh-theme https://raw.githubusercontent.com/denysdovhan/spaceship-zsh-theme/master/spaceship.zsh
-	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 	rm -v .bashrc .bash_logout
 	if [[ $UPDATE == "n" ]]
 	then
@@ -63,14 +62,15 @@ pushd $HOME
 		ln -df $HOME/src/dotfiles/bootstrap.sh bin/dotfiles
 		chmod +x bin/dotfiles
 	fi
-	echo Setting NPM prefix
-	grep -q -F 'prefix=' .npmrc || echo prefix=$HOME/npm-global >> .npmrc
+	#echo Setting NPM prefix
+	#grep -q -F 'prefix=' .npmrc || echo prefix=$HOME/npm-global >> .npmrc
 	echo Symlinking
 	pushd src/dotfiles
 		xstow -t $HOME bash zsh git shell tmux jshint i3
 		xstow -t $HOME/.config .config
 		xstow -t $HOME/bin bin
 	popd
+	curl -o - https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 popd
 
 echo All Done
