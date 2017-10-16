48770b426db68b8b012fd29c790d601f6cc58a23
Adam Shumann <ashumann13@gmail.com>
2017-10-16 16:06:36 -0500

    Move some stuff around

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,9 @@
-#!/usr/bin/env bash
-#if [ "$EUID" -ne 0 ]
-#then
-	# sudo $0 $@
-	# exit
-#fi
-
-
 GUI=n
-APT="apt-transport-https zsh git nano xstow tmux htop curl wget"
+APT="apt-transport-https zsh git nano xstow tmux htop curl wget chsh"
 UPDATE=n
+STOWFILES="bash zsh git shell tmux jshint i3"
+STOWDIRS=".config bin"
+ENSUREDIRS="bin src .config"
 
 [[ "$(basename $0)" == "dotfiles" ]] && UPDATE=y
 
@@ -25,35 +20,29 @@ fi
 sudo apt update
 sudo apt install -y $APT
 
-# http://ags131repo.s3-website-us-east-1.amazonaws.com/repo.ags131.com.gpg.key
-# 'deb https://s3.amazonaws.com/ags131repo/debian main main'
 pushd $HOME
-	mkdir -p bin
-	mkdir -p src
+	for D in $ENSUREDIRS; do mkdir -p $D; done
+
+	# Clone files	
 	if [[ ! -e src/dotfiles ]]
 	then
 		git clone https://github.com/ags131/dotfiles src/dotfiles
 	fi
 
-	usermod -s /bin/zsh $USER
+	# Default shell
+	chsh /bin/zsh
 
+	# oh-my-zsh
 	if [[ ! -e .oh-my-zsh ]]
 	then
 		git clone https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
 	fi
-	#if [[ ! -e src/powerline-shell ]]
-	#then
-	#	git clone https://github.com/milkbikis/powerline-shell src/powerline-shell
-	#	cp src/dotfiles/files/powerline-config.py src/powerline-shell/config.py
-    #
-	#	pushd src/powerline-shell
-	#		./install.py
-	#	popd
-	#fi
-	ZSH_CUSTOM=~/.oh-my-zsh/custom
+	
+	# ZSH THEME
+	ZSH_CUSTOM=$HOME/.zsh_custom
 	mkdir -p $ZSH_CUSTOM/themes
 	curl -o $ZSH_CUSTOM/themes/spaceship.zsh-theme https://raw.githubusercontent.com/denysdovhan/spaceship-zsh-theme/master/spaceship.zsh
-	rm -v .bashrc .bash_logout
+
 	if [[ $UPDATE == "n" ]]
 	then
 		echo Removing symlinks
@@ -61,13 +50,16 @@ pushd $HOME
 		ln -df $HOME/src/dotfiles/bootstrap.sh bin/dotfiles
 		chmod +x bin/dotfiles
 	fi
-	#echo Setting NPM prefix
-	#grep -q -F 'prefix=' .npmrc || echo prefix=$HOME/npm-global >> .npmrc
 	echo Symlinking
 	pushd src/dotfiles
-		xstow -t $HOME bash zsh git shell tmux jshint i3
-		xstow -t $HOME/.config .config
-		xstow -t $HOME/bin bin
+		xstow -t $HOME $STOWFILES
+		for D in ($STOWDIRS)
+		do
+			xstow -t $HOME/$D $D
+		done
+		# xstow -t $HOME bash zsh git shell tmux jshint i3
+		# xstow -t $HOME/.config .config
+		# xstow -t $HOME/bin bin
 	popd
 	curl -o - https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 popd
