d82b30c8f45ace529f9074ee074371c4f4473c6a
Alina Shumann <Alina.Shumann@kyndryl.com>
2025-02-04 11:37:54 -0600

    remove old utils

--- utils2.nix
+++
@@ -1,8 +0,0 @@
-{lib, ...}: {
-  simpleCfg = name: module: {config, ...}:
-    module
-    // {
-      options.acme.${name}.enable = lib.mkEnableOption name;
-      config = lib.mkIf config.acme.${name}.enable module.config;
-    };
-}
