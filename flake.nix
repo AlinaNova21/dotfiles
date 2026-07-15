d1ddd4f4c7ea56d465acd5731847ae3fcdfbe677
AlinaNova21 <alina@alinanova.dev>
2026-07-15 03:08:12 -0500

    fix: pin nixpkgs to f44f7788 to avoid vmTools kernel regression
    The nixos-unstable update after 68d32ed broke disko's vmTools usage
    (kernelModules vs kernel argument change). Pin to known working
    commit before the breaking change (June 11).
    
    Fixes: vmTools error when running home-manager switch or nix develop

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     nixos-images.url = "github:nix-community/nixos-images";
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/f44f7788c891fbe5542177df78374f8cdab10e8f";
     nixos-wsl.url = "github:nix-community/nixos-wsl";
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/0.1";
     sops-nix.url = "github:Mic92/sops-nix";
